9042f409ee4479092ddd9be0499df8b4af6709cd
Amit Raj <rajamitnine@gmail.com>
2025-10-16 15:20:59 +0530

    Primitive Data Type : String

---
+++ Intro_to_JS_Hindi/index.js
@@ -0,0 +1,17 @@
+// Data types are of two types :
+
+// 1. Primitive Data Types
+// 2. Non-primitive Data Types
+
+// 1. Primitive Data Types :
+
+// 1.1 String :
+let mentor1 = "Prabir";
+let mentor2 = "Mithun S";
+let mentor3 = `Vishwa Mohan Singh`;
+
+console.log(mentor1, typeof mentor1);
+console.log(mentor2, typeof mentor2);
+console.log(mentor3, typeof mentor3);
+
+// ---------------------------------------
